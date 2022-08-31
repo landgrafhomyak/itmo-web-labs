0d0735cda718ac97da25288c6a526ae39cb2e616
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2022-08-31 18:42:16 +0300

    Minimal sources

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Лабоаторная №1</title>
+    <link rel="stylesheet" href="common.css">
+</head>
+<body>
+ HelloWorld
+</body>
+</html>
